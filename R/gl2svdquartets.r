82f6a61f2b78ae9464ecd8a3fc38cee4498b702f
Arthur Georges <Arthur Georges>
2018-04-23 12:10:48 +1000

    Added option to analyse with ambiguity codes, as per advice from Swofford.

--- R/gl2svdquartets.r
+++ R/gl2svdquartets.r
@@ -1,13 +1,20 @@
 #' Convert a genlight object to nexus format PAUP SVDquartets
 #'
-#' Package SNPRelate relies on a bit-level representation of a SNP dataset that competes with \{adegenet\} genlight
-#' objects and associated files. This function saves a genlight object to a gds format file.
+#' The output nexus file contains the snp data in one of two forms, depending upon what you regard
+#' as most appropriate. One form, that used by Chifman and Kubatko, has two lines per individual,
+#' one providing the reference SNP the second providing the alternate SNP (method=1). 
+#' A second form, recommended by Dave Swofford, has
+#' a single line per individual, resolving heterozygous SNPs by replacing them with standard
+#' ambiguity codes (method=2).
 #'
 #' Reference: Chifman, J. and L. Kubatko. 2014. Quartet inference from SNP data under the coalescent, Bioinformatics, 30: 3317-3324
 #' 
 #' 
 #' @param x -- name of the genlight object containing the SNP data [required]
 #' @param outfile -- file name of the output file (including extension).
+#' @param method -- method = 1, nexus file with two lines per individual; method = 2, nexus
+#' file with one line per individual, ambiguity codes [default 2]
+#' @param v -- verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
 #' @return NULL
 #' @export
 #' @importFrom
@@ -15,17 +22,18 @@
 #' @examples
 #' gl2svdquartets(testset.gl)
 
-gl2svdquartets <- function(x, outfile="svd.nex") {
+gl2svdquartets <- function(x, outfile="svd.nex", method=2, v=2) {
   
-  if (v > 0) {cat(paste("Starting gl2svdquartets: Create nexus file suitable for svdquartets\n\n"))}
+  if (v > 0) {cat(paste("Starting gl2svdquartets: Create nexus file\n\n"))}
+  if (v > 1) {cat(paste("    Extacting SNP bases and creating records for each individual\n"))}
 
 # Extract the reference base and the alternate base for each locus
-  v <- as.character(x@other$loc.metrics$SNP)
-  v <- strsplit(v,":")
-  v <- unlist(lapply(v, `[`, 2))
-  v <- strsplit(v,">")
-  ref <- unlist(lapply(v, `[`, 1))
-  alt <- unlist(lapply(v, `[`, 2))
+  snp <- as.character(x@other$loc.metrics$SNP)
+  snp <- strsplit(snp,":")
+  snp <- unlist(lapply(snp, `[`, 2))
+  snp <- strsplit(snp,">")
+  ref <- unlist(lapply(snp, `[`, 1))
+  alt <- unlist(lapply(snp, `[`, 2))
   
 # Sort the data on population
   df <- data.frame(as.matrix(x))
@@ -34,45 +42,56 @@ gl2svdquartets <- function(x, outfile="svd.nex") {
   indlabels <- df[,1]
   poplabels <- df[,2]
   m <- df[,3:(nLoc(x)+2)]
+  
+  # Create a lookup table for the ambiguity codes
+  #     A  T  G  C
+  #  A  A  W  R  M)
+  #  T  W  T  K  Y
+  #  G  R  K  G  S
+  #  C  M  Y  S  C  
+  
+  conversion <- matrix(c("A","W", "R","M", "W", "T", "K", "Y", "R", "K", "G", "S", "M", "Y", "S", "C"),nrow=4, ncol=4)
+  colnames(conversion) <- c("A","T","G","C")
+  rownames(conversion) <- colnames(conversion)
 
-  #Initialize The first base of the sequence for each individual  
+  # Add individual labels to sequences
   refseq <- array(data=NA,dim=nInd(x))
   altseq <- array(data=NA,dim=nInd(x))
-  for (ind in 1:nInd(x)) {
-    if (is.na(m[ind,1])) {
-      refseq[ind] <- "?"; altseq[ind] <- "?"
-    } else if (m[ind,1] == 0) {
-      refseq[ind] <- ref[1]; altseq[ind] <- ref[1]
-    } else if (m[ind,1] == 1) {
-      refseq[ind] <- ref[1]; altseq[ind] <- alt[1]
-    } else if (m[ind,1] == 2) {
-      refseq[ind] <- alt[1]; altseq[ind] <- alt[1]
-    } else {
-      cat ("Fatal Error: SNP must be scored as 0, 1, 2 or NA\n"); stop()
+  ambseq <- array(data=NA,dim=nInd(x))
+  if (method == 1) {
+    for (ind in 1:nInd(x)) {
+      refseq[ind] <- paste0(indlabels[ind],"_1   ")
+      altseq[ind] <- paste0(indlabels[ind],"_2   ")
     }
-  }
-  
+  } else {
+    for (ind in 1:nInd(x)) {
+      ambseq[ind] <- paste0(indlabels[ind],"   ")
+    }
+  } 
+
 # progressively add the bases  
   for (ind in 1:nInd(x)) {
     for (loc in 2:nLoc(x)){
     if (is.na(m[ind,loc])) {
-      refseq[ind] <- paste0(refseq[ind],"?"); altseq[ind] <- paste0(altseq[ind],"?")
+      refseq[ind] <- paste0(refseq[ind],"?")
+      altseq[ind] <- paste0(altseq[ind],"?")
+      ambseq[ind] <- paste0(ambseq[ind],"?")
     } else if (m[ind,loc] == 0) {
-      refseq[ind] <- paste0(refseq[ind],ref[loc]); altseq[ind] <- paste0(altseq[ind],ref[loc])
+      refseq[ind] <- paste0(refseq[ind],ref[loc])
+      altseq[ind] <- paste0(altseq[ind],ref[loc])
+      ambseq[ind] <- paste0(ambseq[ind],ref[loc])
     } else if (m[ind,loc] == 1) {
-      refseq[ind] <- paste0(refseq[ind],ref[loc]); altseq[ind] <- paste0(altseq[ind],alt[loc])
+      refseq[ind] <- paste0(refseq[ind],ref[loc])
+      altseq[ind] <- paste0(altseq[ind],alt[loc])
+      ambseq[ind] <- paste0(ambseq[ind],conversion[ref[loc], alt[loc]])
     } else if (m[ind,loc] == 2) {
-      refseq[ind] <- paste0(refseq[ind],alt[loc]); altseq[ind] <- paste0(altseq[ind],alt[loc])
+      refseq[ind] <- paste0(refseq[ind],alt[loc])
+      altseq[ind] <- paste0(altseq[ind],alt[loc])
+      ambseq[ind] <- paste0(ambseq[ind],alt[loc])
     } else {
       cat ("Fatal Error: SNP must be scored as 0, 1, 2 or NA\n"); stop()
     }
     }
-  }
-  
-# Add individual names to sequences (should have done this first)
-  for (ind in 1:nInd(x)) {
-    refseq[ind] <- paste0(indlabels[ind],"_1   ",refseq[ind])
-    altseq[ind] <- paste0(indlabels[ind],"_2   ",altseq[ind])
   }
   
 # Create the taxpartition (popname : 25-60)
@@ -87,16 +106,23 @@ gl2svdquartets <- function(x, outfile="svd.nex") {
   plabels <- unique(poplabels)
   
 # Create the svd file
+  if (v > 1) {cat(paste("    Writing results to nexus file",outfile,"\n"))}
   sink(outfile)
   cat("#nexus\n")
   cat("BEGIN DATA;\n")
   cat(paste0("     dimensions ntax = ",2*nInd(x)," nchar = ",nLoc(x)," ;\n"))
   cat("     format datatype = dna gap = - ;\n\n")
   cat("matrix\n")
-  for (i in 1:nInd(x)){
-    cat(paste0(refseq[i],"\n"))
-    cat(paste0(altseq[i],"\n"))
-  }
+  if (method == 1) {
+    for (i in 1:nInd(x)){
+      cat(paste0(refseq[i],"\n"))
+      cat(paste0(altseq[i],"\n"))
+    }
+  } else {
+    for (i in 1:nInd(x)){
+      cat(paste0(ambseq[i],"\n"))
+    }
+  }  
   cat(";\n")
   cat("end;\n\n")
   cat("begin sets;\n")
@@ -124,6 +150,7 @@ gl2svdquartets <- function(x, outfile="svd.nex") {
   
   sink()
   
+  if (v > 2) {cat(paste("    Records written to",outfile,":",nInd(x),"\n"))}
   if (v > 0) {cat("gl2svdquartets Completed\n")}
   
   return(NULL)
