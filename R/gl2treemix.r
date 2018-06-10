deb16c40ff6660346f835df7f0c67b0b8626afb3
arthurgeorges <georges@aerg.canberra.edu.au>
2018-06-10 17:51:40 +1000

    Added a script gl2treemix.r and associated files for displaying the output.

---
+++ R/gl2treemix.r
@@ -0,0 +1,59 @@
+#' Convert a genlight object to a treemix input file
+#' 
+#' The output file contains the snp data in the format expected by treemix -- see the treemix manual. The file needs to be gzipped before it will be recognised
+#' by treemix. Plotting functions can be obtained using source("C:/workspace/R/dartR/R/plotting_funcs.R").  
+#'
+#' Reference: Pickrell and Pritchard (2012). Inference of population splits and mixtures from genome-wide allele frequency data. PLoS Genetics https://doi.org/10.1371/journal.pgen.1002967
+#' 
+#' @param x -- name of the genlight object containing the SNP data [required]
+#' @param outfile -- file name of the output file (including extension).
+#' @param v -- verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
+#' @return NULL
+#' @export
+#' @importFrom
+#' @author Arthur Georges (glbugs@aerg.canberra.edu.au)
+#' @examples
+#' gl2treemix(testset.gl)
+
+gl2treemix <- function(x, outfile="treemix_input.gz", v=2) {
+  
+  if (v > 0) {cat(paste("Starting gl2treemix: Create treemix input file\n\n"))}
+
+# Calculate allele counts
+  
+  freq <- gl.percent.freq(x)
+  freq$ref <- freq$nobs*2-freq$sum
+  freq$alt <- freq$sum
+  freq$sum <- NULL
+  freq$nobs <- NULL
+  freq$nmissing <- NULL
+  freq$frequency <- NULL
+  freq$n <- NULL
+
+# Output the file
+  if (v > 1) {cat(paste("    Writing results to treemix input file",outfile,"\n"))}
+  sink(outfile)
+
+  cat (unique(as.character(freq$popn)),"\n")
+  k <- 1
+  for (j in 1:nLoc(x)) {
+  for (i in k:(k+nPop(x)-1)) {
+      cat(paste0(freq$ref[i],",",freq$alt[i])," ")
+  }
+  cat("\n")
+  k <- k + nPop(x)
+  }
+  
+  sink()
+  
+  if (v > 2) {cat(paste("    Records written to",outfile,":",nInd(x),"\n"))}
+  if (v > 0) {
+    cat("gl2treemix Completed\n")
+    cat("Output file will need to be zipped before input to treemix\n")
+  }
+  
+  return(NULL)
+
+}
+
+
