0d4bd818c76583b7d9e7f7d4d163a22f7e254676
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2018-09-24 17:37:21 +1000

    gl.diversity with sds

--- R/gl.diversity.r
+++ R/gl.diversity.r
@@ -7,11 +7,12 @@
 #' 
 #' @return a list of entropy indices for each level of q and equivalent numbers for alpha and beta diversity.
 #' @export
-#' @author Bernd Gruber (glbugs@@aerg.canberra.edu.au)
+#' @author Bernd Gruber (glbugs@@aerg.canberra.edu.au), Contributors: William B. Sherwin, Alexander Sentinella 
 #' @references 
 #' Sherwin, W.B., Chao, A., Johst, L., Smouse, P.E. (2017). Information Theory Broadens the Spectrum of Molecular Ecology and Evolution. TREE 32(12) 948-963. doi:10.1016/j.tree.2017.09.12
 #' 
 #' Chao et al. 2014
+#' 
  
 
 ### To be done:
@@ -19,47 +20,77 @@
 # adjust calculation of betas for population sizes (switch)
 
 
-gl.diversity <- function(gl, spectrumplot=TRUE) {
+gl.diversity <- function(gl, spectrumplot=TRUE, verbose=2) {
 
 if  (is.null(pop(gl)))  pop(gl)<- factor(rep("all", nInd(gl)))
 
 #split in pops
 pops <- seppop(gl)
      
-  ##0Halpha (average number of alleles, ignoring missing values)
 
-nlocpop <- lapply(pops, function(x) sum(!is.na(colMeans(as.matrix(x)))))
+
+#number of missing loci
+
+nlocpop <- lapply(pops, function(x) sum(!is.na(colMeans(as.matrix(x), na.rm = T))))
 nlocpop$totalloc <- nLoc(gl)
 
-zero_H_alpha <- lapply(pops, function(x) mean(((colMeans(as.matrix(x), na.rm=T) %% 2)>0)+1-1, na.rm = T))
 
 
-one_H_alpha <- lapply(pops, function(x) {
+##0Halpha (average number of alleles, ignoring missing values and sd)
+zero_H_alpha_es <- lapply(pops, function(x) {
+  dummys <- ((colMeans(as.matrix(x), na.rm=T) %% 2)>0)+1-1 
+  return( list(estH=mean(dummys,  na.rm = T), sdH=sd(dummys, na.rm=T),estD =mean(dummys, na.rm = T)+1, sdD=sd(dummys, na.rm = T)+1)) 
+})
+zero_H_alpha <- unlist(lapply(zero_H_alpha_es, function(x) x[[1]]))
+zero_H_alpha_sd <- unlist(lapply(zero_H_alpha_es, function(x) x[[2]]))
+zero_D_alpha <- unlist(lapply(zero_H_alpha_es, function(x) x[[3]]))
+zero_D_alpha_sd <- unlist(lapply(zero_H_alpha_es, function(x) x[[4]]))
+
+
+
+### one_H_alpha
+one_H_alpha_es <- lapply(pops, function(x) {
   p<- colMeans(as.matrix(x), na.rm = T)/2
   p <- p[!is.na(p)] #ignore loci with just missing data
   logp <- ifelse(!is.finite(log(p)), 0, log(p))
   log1_p <- ifelse(!is.finite(log(1-p)), 0, log(1-p))
-  return(-mean(p*logp + (1-p)*log1_p))
+  
+  dummys <- -(p*logp + (1-p)*log1_p)
+  
+  return(list(estH=mean(dummys), sdH=sd(dummys), estD=mean(exp(dummys)), sdD=sd(exp(dummys)), dummys=dummys))
 })
+one_H_alpha <- unlist(lapply(one_H_alpha_es, function(x) x[[1]]))
+one_H_alpha_sd <- unlist(lapply(one_H_alpha_es, function(x) x[[2]]))
+one_D_alpha <- unlist(lapply(one_H_alpha_es, function(x) x[[3]]))
+one_D_alpha_sd <- unlist(lapply(one_H_alpha_es, function(x) x[[4]]))
 
-two_H_alpha <- lapply(pops, function(x) {
+
+#two_H_alpha
+two_H_alpha_es <- lapply(pops, function(x) {
   p<- colMeans(as.matrix(x), na.rm = T)/2
   p <- p[!is.na(p)] #ignore loci with just missing data
-  return(mean(1-(p*p+(1-p)*(1-p)))  )
+  dummys <- (1-(p*p+(1-p)*(1-p)))
+  
+  return(list(estH=mean(dummys), sdH=sd(dummys), estD=mean(1/(1-dummys)), sdD=sd(1/(1-dummys)) , dummys=dummys ))
 })
+two_H_alpha <- unlist(lapply(two_H_alpha_es, function(x) x[[1]]))
+two_H_alpha_sd <- unlist(lapply(two_H_alpha_es, function(x) x[[2]]))
+two_D_alpha <- unlist(lapply(two_H_alpha_es, function(x) x[[3]]))
+two_D_alpha_sd <- unlist(lapply(two_H_alpha_es, function(x) x[[4]]))
 
+
+
+#initiallize betas as NA 
 mat_zero_H_beta <- NA
 mat_one_H_beta <- NA
 mat_two_H_beta <- NA
-
 npops <- length(pops)
 
 if (npops>1)
 {
 pairs <- t(combn(npops,2))
 
-#missing loci 
-#zero_H_beta
+### pairwise missing loci 
 nlocpairpop <- apply(pairs,1, function(x)  {
   pop1 <- pops[[x[1]]]
   pop2 <- pops[[x[2]]]
@@ -67,21 +98,14 @@ nlocpairpop <- apply(pairs,1, function(x)  {
   pp2 <- colMeans(as.matrix(pop2), na.rm = T)/2
   index <- !is.na(pp1) & !is.na(pp2)
   return(sum(index))
-  
 } )
-
-
-
 mat_nloc_pops <- matrix(NA, nrow = npops, ncol = npops)
 mat_nloc_pops[lower.tri(mat_nloc_pops)] <- nlocpairpop
 colnames(mat_nloc_pops) <- rownames(mat_nloc_pops) <- names(pops)
 
 
-
-
-
 #zero_H_beta
-zero_H_beta <- apply(pairs,1, function(x)  {
+zero_H_beta_es <- apply(pairs,1, function(x)  {
   
   pop1 <- pops[[x[1]]]
   pop2 <- pops[[x[2]]]
@@ -95,65 +119,98 @@ zero_H_beta <- apply(pairs,1, function(x)  {
   index <- !is.na(pp1) & !is.na(pp2)
   pp1 <- pp1[index]
   pp2 <- pp2[index]
-  return(mean(abs(pp1-pp2)))
+  
+  dummys <- abs(pp1-pp2)
+  #  mat_zero_D_beta <- mat_zero_H_beta+1
+  return(list(estH=mean(dummys), sdH=sd(dummys),estD= mean(dummys)+1,sdD =sd(dummys)))
   
 } )
 
-
-
+zero_H_beta <- unlist(lapply(zero_H_beta_es, function(x) x[[1]]))
+zero_H_beta_sd<-unlist(lapply(zero_H_beta_es, function(x) x[[2]]))
+zero_D_beta <- unlist(lapply(zero_H_beta_es, function(x) x[[3]]))
+zero_D_beta_sd<-unlist(lapply(zero_H_beta_es, function(x) x[[4]]))
 
 mat_zero_H_beta <- matrix(NA, nrow = npops, ncol = npops)
 mat_zero_H_beta[lower.tri(mat_zero_H_beta)] <- zero_H_beta
+mat_zero_H_beta[pairs] <- zero_H_beta_sd
 colnames(mat_zero_H_beta) <- rownames(mat_zero_H_beta) <- names(pops)
+
+mat_zero_D_beta <- matrix(NA, nrow = npops, ncol = npops)
+mat_zero_D_beta[lower.tri(mat_zero_D_beta)] <- zero_D_beta
+mat_zero_D_beta[pairs] <- zero_D_beta_sd
+colnames(mat_zero_D_beta) <- rownames(mat_zero_D_beta) <- names(pops)
 
 
 #one_H_beta
 # calculate one_H_alpha_all for combined pops
 p<- colMeans(as.matrix(gl), na.rm = T)/2
-p <- p[!is.na(p)] #ignore loci with just missing data
+i1 <- !is.na(p)#ignore loci with just missing data
+#p <- p[!is.na(p)] #ignore loci with just missing data
 logp <- ifelse(!is.finite(log(p)), 0, log(p))
 log1_p <- ifelse(!is.finite(log(1-p)), 0, log(1-p))
-one_H_beta_all <- -mean(p*logp + (1-p)*log1_p)
+one_H_alpha_all <- -(p*logp + (1-p)*log1_p)
 
-
-one_H_beta <- apply(pairs,1, function(x) {
-  return(one_H_beta_all-mean(c(one_H_alpha[[x[1]]], one_H_alpha[[x[2]]])))
+one_H_beta_es <- apply(pairs,1, function(x) { 
+  i2 <- !is.na(colMeans(as.matrix(pops[[x[1]]]), na.rm = T)/2)
+  i3 <- !is.na(colMeans(as.matrix(pops[[x[2]]]), na.rm = T)/2)
+  index <- i1 & i2 & i3
+  dummys <- one_H_alpha_all[index]-(one_H_alpha_es[[x[1]]]$dummys[index]+ one_H_alpha_es[[x[2]]]$dummys[index])/2
+  return(list(estH=mean(dummys), sdH=sd(dummys), estD=mean(exp(dummys)), sdD=sd(exp(dummys))))
 })
+
+one_H_beta <- unlist(lapply(one_H_beta_es, function(x) x[[1]]))
+one_H_beta_sd<-unlist(lapply(one_H_beta_es, function(x) x[[2]]))
+one_D_beta <- unlist(lapply(one_H_beta_es, function(x) x[[3]]))
+one_D_beta_sd<-unlist(lapply(one_H_beta_es, function(x) x[[4]]))
+
 mat_one_H_beta <- matrix(NA, nrow = npops, ncol = npops)
 mat_one_H_beta[lower.tri(mat_one_H_beta)] <- one_H_beta
+mat_one_H_beta[pairs] <- one_H_beta_sd
 colnames(mat_one_H_beta) <- rownames(mat_one_H_beta) <- names(pops)
+
+mat_one_D_beta <- matrix(NA, nrow = npops, ncol = npops)
+mat_one_D_beta[lower.tri(mat_one_D_beta)] <- one_D_beta
+mat_one_D_beta[pairs] <- one_D_beta_sd
+colnames(mat_one_D_beta) <- rownames(mat_one_D_beta) <- names(pops)
+
 
 #two_H_beta
 # calculate two_H_alpha_all for combined pops
 p<- colMeans(as.matrix(gl), na.rm = T)/2
-p <- p[!is.na(p)] #ignore loci with just missing data
-two_H_beta_all <- mean(1-(p*p+(1-p)*(1-p)))  
+i1 <- !is.na(p)#ignore loci with just missing data
+#p <- p[!is.na(p)] #ignore loci with just missing data
+two_H_alpha_all <- (1-(p*p+(1-p)*(1-p)))  
 
-two_H_beta <- apply(pairs,1, function(x) {
-  m2Ha <- mean(c(two_H_alpha[[x[1]]], two_H_alpha[[x[2]]]))
+two_H_beta_es <- apply(pairs,1, function(x) {
+  
+  i2 <- !is.na(colMeans(as.matrix(pops[[x[1]]]), na.rm = T)/2)
+  i3 <- !is.na(colMeans(as.matrix(pops[[x[2]]]), na.rm = T)/2)
+  index <- i1 & i2 & i3
+  
+  m2Ha <- (two_H_alpha_es[[x[1]]]$dummys[index]+ two_H_alpha_es[[x[2]]]$dummys[index])/2
+  dummys <- ((two_H_alpha_all[index]-m2Ha[index])/(1-m2Ha))*(npops/(npops-1))
 #Johst-D
-    return(  ((two_H_beta_all-m2Ha)/(1-m2Ha))*(npops/(npops-1)))
-  #GST
+    return(list(estH=mean(dummys), sdH=sd(dummys), estD=mean(exp(dummys)), sdD=sd(exp(dummys))) )
  # return((two_H_beta_all-m2Ha)/two_H_beta_all  )
-  
-  
 })
-  mat_two_H_beta <- matrix(NA, nrow = npops, ncol = npops)
-  mat_two_H_beta[lower.tri(mat_two_H_beta)] <- two_H_beta
-  colnames(mat_two_H_beta) <- rownames(mat_two_H_beta) <- names(pops)
 
-  mat_zero_D_beta <- mat_zero_H_beta+1
-  mat_one_D_beta <-  exp(mat_one_H_beta)
-  mat_two_D_beta <-  exp(mat_two_H_beta)
+two_H_beta <- unlist(lapply(two_H_beta_es, function(x) x[[1]]))
+two_H_beta_sd<-unlist(lapply(two_H_beta_es, function(x) x[[2]]))
+two_D_beta <- unlist(lapply(two_H_beta_es, function(x) x[[3]]))
+two_D_beta_sd<-unlist(lapply(two_H_beta_es, function(x) x[[4]]))
+
+mat_two_H_beta <- matrix(NA, nrow = npops, ncol = npops)
+mat_two_H_beta[lower.tri(mat_two_H_beta)] <- two_H_beta
+mat_two_H_beta[pairs] <- two_H_beta_sd
+colnames(mat_two_H_beta) <- rownames(mat_two_H_beta) <- names(pops)
+
+mat_two_D_beta <- matrix(NA, nrow = npops, ncol = npops)
+mat_two_D_beta[lower.tri(mat_two_D_beta)] <- two_D_beta
+mat_two_D_beta[pairs] <- two_D_beta_sd
+colnames(mat_two_D_beta) <- rownames(mat_two_D_beta) <- names(pops)
 
 } # npops>1  
-
-#Calculate hill numbers
-zero_D_alpha <- unlist(zero_H_alpha)+1
-one_D_alpha <- exp(unlist(one_H_alpha))
-two_D_alpha <- 1/(1-unlist(two_H_alpha))
-
-
 
 
 if (spectrumplot)
@@ -161,13 +218,33 @@ if (spectrumplot)
 
 fs <- cbind(zero_D_alpha, one_D_alpha, two_D_alpha) 
 cx <- max(1-(max(-12+nrow(fs),0)*0.025),0.5)
-bb<-  barplot(fs, beside = T, names.arg = rep(rownames(fs),3), ylim=c(1,2.2), main="q-profile", col=rainbow(npops), las=2, xpd=FALSE, cex.names=cx)
+bb<-  barplot(fs, beside = T, names.arg = rep(rownames(fs),3), ylim=c(1,2.15), main="q-profile", col=rainbow(npops), las=2, xpd=FALSE, cex.names=cx)
  text(colMeans(bb), rep(2.1,3), labels = c("q=0", "q=1", "q=2")) 
 }
-return(list(nlocpop=unlist(nlocpop), nlocpairpop = mat_nloc_pops,
-  zero_H_alpha= unlist(zero_H_alpha), one_H_alpha=unlist(one_H_alpha),two_H_alpha=unlist(two_H_alpha) ,
-  zero_D_alpha= zero_D_alpha, one_D_alpha=one_D_alpha,two_D_alpha=two_D_alpha ,
-  zero_H_beta=mat_zero_H_beta, one_H_beta=mat_one_H_beta, two_H_beta=mat_two_H_beta, zero_D_beta = mat_zero_D_beta, one_D_beta = mat_one_D_beta, two_D_beta = mat_two_D_beta))
+return(list(  nlocpop=unlist(nlocpop), 
+              nlocpairpop = mat_nloc_pops,
+              
+              zero_H_alpha=    zero_H_alpha,
+              zero_H_alpha_sd= zero_H_alpha_sd,
+              one_H_alpha=     one_H_alpha,
+              one_H_alpha_sd=  one_H_alpha_sd,
+              two_H_alpha=     two_H_alpha,
+              two_H_alpha_sd=  two_H_alpha_sd,
+            
+              zero_D_alpha   = zero_D_alpha, 
+              zero_D_alpha_sd= zero_D_alpha_sd,
+              one_D_alpha    = one_D_alpha,
+              one_D_alpha_sd = one_D_alpha_sd,
+              two_D_alpha    = two_D_alpha,
+              two_D_alpha_sd = two_D_alpha_sd,
+            
+              zero_H_beta=mat_zero_H_beta, 
+              one_H_beta=mat_one_H_beta, 
+              two_H_beta=mat_two_H_beta, 
+            
+              zero_D_beta = mat_zero_D_beta, 
+              one_D_beta = mat_one_D_beta, 
+              two_D_beta = mat_two_D_beta))
 }
 
 
