d81f2f423a759d0c58d34ffc9356703bcc6356f8
arthurgeorges <georges@aerg.canberra.edu.au>
2018-12-03 08:45:14 +1100

    Minor changes to gl.read.csv

--- R/gl.read.csv.r
+++ R/gl.read.csv.r
@@ -2,8 +2,12 @@
 #'
 #' This script takes SNP genotypes from a csv file, combines them with individual and locus metadata and creates a genlight object.
 #' 
-#' The data need to be in one of two forms. SNPs can be coded 0 for homozygous reference, 2 for homozygous alternate, and 1 for heterozygous with NA for missing values, or
+#' The SNP data need to be in one of two forms. SNPs can be coded 0 for homozygous reference, 2 for homozygous alternate, and 1 for heterozygous with NA for missing values; or
 #' it can be coded A/A, A/C, C/T, G/A etc, with -/- as missing. Other formats will thow an error.
+#' 
+#' The individual metadata needs to be in a csv file, with headings, with a mandatory id column corresponding exactly to the identities provided with the SNP data. The locus metadata needs
+#' to be in a csv file with headings. Note that the locus metadata will be complemented by calculable statistics corresponding to those that would be provided by Diversity Arrays
+#' Technology (e.g. CallRate)
 #'
 #' @param filename -- name of the csv file containing the SNP genotypes [required]
 #' @param ind.labels -- if TRUE, then the first column of the csv file is the individual/specimen/sample labels [default TRUE]
@@ -11,11 +15,10 @@
 #' @param ind.metafile -- name of the csv file containing the metadata for individuals [optional]
 #' @param ind.metafile -- name of the csv file containing the metadata for individuals [optional]
 #' @param v -- verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
-#' @return a genlight object
+#' @return a genlight object with the SNP data and associated metadata included.
+#' @import adegenet
 #' @export
 #' @author Arthur Georges (Post to \url{https://groups.google.com/d/forum/dartr})
-#' @examples
-#'    gl <- gl.read.csv(filename="test.csv"]
 
 gl.read.csv <- function(filename, ind.labels=TRUE, loc.labels=TRUE, ind.metafile=NULL, loc.metafile=NULL, v=2){
   
@@ -33,6 +36,7 @@ gl.read.csv <- function(filename, ind.labels=TRUE, loc.labels=TRUE, ind.metafile
   }
   
 # DO THE JOB
+  #FIRST THE SNP DATA
 
   # Create the SNP data matrix, indNames and LocNames
   df0 <- read.csv(file=filename,header=FALSE)
@@ -120,25 +124,36 @@ gl.read.csv <- function(filename, ind.labels=TRUE, loc.labels=TRUE, ind.metafile
       data <- apply(data,2,as.numeric)
   }    
       
-      gl <- as.genlight(data)
-      locNames(gl) <- loci
-      indNames(gl) <- individuals
-      if(!is.null(ind.metafile)){
-        ind.metadata <- read.csv(file=ind.metafile,header=TRUE)
-        gl@other$ind.metrics <- ind.metadata
-      } else {
-        gl@other$ind.metrics$id <- individuals
-        gl@other$ind.metrics$pop <- array(NA,nInd(gl))
-      }
-      if(!is.null(loc.metafile)){
-        loc.metadata <- read.csv(file=loc.metafile,header=TRUE)
-        gl@other$loc.metrics <- loc.metadata
-        gl <- gl.recalc.metrics(gl)
-      } else {
-        gl <- gl.recalc.metrics(gl)
-      }
+  gl <- as.genlight(data)
+  locNames(gl) <- loci
+  indNames(gl) <- individuals
 
-  # FLAG SCRIPT END
+  # SECOND THE LOCUS METADATA
+  if(!is.null(loc.metafile)){
+    loc.metadata <- read.csv(file=loc.metafile,header=TRUE)
+    gl@other$loc.metrics <- loc.metadata
+    gl <- gl.recalc.metrics(gl)
+  } else {
+    gl <- gl.recalc.metrics(gl)
+  }
+  if (v >= 2){cat(paste(" Added or updated ",names(gl@other$loc.metrics)," to the other$ind.metrics slot.\n"))}
+  
+  # THIRD THE INDIVIDUAL METADATA
+  if(!is.null(ind.metafile)){
+    ind.metadata <- read.csv(file=ind.metafile,header=TRUE)
+    if (!("id" %in% names(ind.metadata))) {cat("Fatal Error: mandatory id column absent from the individual metadata file\n")}
+    if (!("pop" %in% names(ind.metadata))) {
+      cat("  Warning: pop column absent from the individual metadata file, setting to NA\n")
+      gl@other$ind.metrics$pop <- array(NA,nInd(gl))
+    }
+    gl@other$ind.metrics <- ind.metadata
+  } else {
+    gl@other$ind.metrics$id <- individuals
+    gl@other$ind.metrics$pop <- array(NA,nInd(gl))
+  }
+  if (v >= 2){cat(paste(" Added ",names(gl@other$ind.metrics)," to the other$ind.metrics slot.\n"))}
+ 
+# FLAG SCRIPT END
       
       if (v >= 1) {
         cat("Completed gl.read.csv\n\n")
