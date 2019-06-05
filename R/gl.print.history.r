5be38ce23eef824301e575813f8b115050224583
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2019-06-05 14:38:10 +1000

    added history

--- R/gl.print.history.r
+++ R/gl.print.history.r
@@ -1,5 +1,22 @@
-
+#' Prints history of a genlight object
+#' 
+#' @param x -- a genlight object (with history [optional])
+#' @param history -- [optional] either a link to a history slot (gl\@other$history), or a vector indicating which part of the history of x is used [c(1,3,4) uses the first, third and forth entry from x\@other$history]. If no history is provided the complete history of x is used (recreating the identical object x).
+#' @return prints a table with all history records. Currently the style cannot be changed.
+#' @export
 #' @importFrom gridExtra grid.table ttheme_default
+#' @author Bernd Gruber (bugs? Post to \url{https://groups.google.com/d/forum/dartr})
+#' @examples 
+#' dartfile <- system.file("extdata","testset_SNPs_2Row.csv", package="dartR")
+#' metadata <- system.file("extdata","testset_metadata.csv", package="dartR")
+#' gl <- gl.read.dart(dartfile, ind.metafile = metadata, probar=FALSE) 
+#' gl2 <- gl.filter.callrate(gl, method="loci", threshold=0.9)
+#' gl3 <- gl.filter.callrate(gl2, method="ind", threshold=0.95)
+#' #Now "replay" part of the history "onto" another genlight object
+#' bc.fil <- gl.play.history(bandicoot.gl, history=gl3@other$history[c(2,3)], verbose=1)
+#' gl.print.history(bc.fil)
+
+
 
 
 gl.print.history<- function(x = NULL, history = NULL)
