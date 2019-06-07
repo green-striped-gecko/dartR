d135561141529d7b2f560ae757c6fe18e3a9c3af
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2019-06-07 10:14:33 +1000

    fixed minor error in gl.nyhybrid and gl.cloneid

--- R/gl.nhybrids.r
+++ R/gl.nhybrids.r
@@ -88,7 +88,7 @@ gl.nhybrids <- function(gl, outfile="nhyb.txt", outpath=tempdir(),
     cat("Fatal Error: genlight object required!\n"); stop("Execution terminated\n")
   }
   # Work around a bug in adegenet if genlight object is created by subsetting
-  if (nLoc(gl)!=nrow(x@other$loc.metrics)) { stop("The number of rows in the loc.metrics table does not match the number of loci in your genlight object!")  }
+  if (nLoc(gl)!=nrow(gl@other$loc.metrics)) { stop("The number of rows in the loc.metrics table does not match the number of loci in your genlight object!")  }
 
   
   if (verbose < 0 | verbose > 5){
