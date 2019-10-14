7b67882244e4c9c159403065b574e99b6a87367d
arthurgeorges <georges@aerg.canberra.edu.au>
2019-10-15 09:49:12 +1100

    Modified to handle tag P/A data (SilicoDArT) and to update the flags.

--- R/utils.recalc.freqhets.r
+++ R/utils.recalc.freqhets.r
@@ -19,38 +19,45 @@
 utils.recalc.freqhets <- function(x, verbose=2) {
 
 # TIDY UP FILE SPECS
-
+  
+  build <- "Jacob"
   funname <- match.call()[[1]]
-
+  hold <- x
+  # Note draws upon and modifies the loc.metrics.flags for freqhets.
+  
 # FLAG SCRIPT START
-
+  
   if (verbose < 0 | verbose > 5){
     cat("  Warning: Parameter 'verbose' must be an integer between 0 [silent] and 5 [full report], set to 2\n")
     verbose <- 2
   }
-
-  if (verbose > 0) {
-    cat("Starting",funname,"\n")
+  
+  if (verbose >= 1){
+    cat("Starting",funname,"[ Build =",build,"]\n")
   }
-
+  
 # STANDARD ERROR CHECKING
   
   if(class(x)!="genlight") {
     cat("  Fatal Error: genlight object required!\n"); stop("Execution terminated\n")
   }
-  # Work around a bug in adegenet if genlight object is created by subsetting
-      if (nLoc(x)!=nrow(x@other$loc.metrics)) { stop("The number of rows in the loc.metrics table does not match the number of loci in your genlight object!")  }
-
-  # Set a population if none is specified (such as if the genlight object has been generated manually)
-    if (is.null(pop(x)) | is.na(length(pop(x))) | length(pop(x)) <= 0) {
-      if (verbose >= 2){ cat("  Population assignments not detected, individuals assigned to a single population labelled 'pop1'\n")}
-      pop(x) <- array("pop1",dim = nInd(x))
-      pop(x) <- as.factor(pop(x))
+  
+  if (verbose >= 2){
+    if (all(x@ploidy == 1)){
+      stop("  Detected Presence/Absence (SilicoDArT) data ",funname," applies only to SNP data!\n")
+    } else if (all(x@ploidy == 2)){
+      cat("  Processing a SNP dataset\n")
+    } else {
+      stop("Fatal Error: Ploidy must be universally 1 (fragment P/A data) or 2 (SNP data)")
     }
-    
-  # Check for monomorphic loci
-    tmp <- gl.filter.monomorphs(x,verbose=0)
-    if ((nLoc(tmp) < nLoc(x)) & verbose >= 2) {cat("  Warning: genlight object contains monomorphic loci\n")}
+  }
+  
+  # Check monomorphs have been removed up to date
+  if (x@other$loc.metrics.flags$monomorphs == FALSE){
+    if (verbose >= 2){
+      cat("  Warning: Dataset contains monomorphic loci which will be included in the ",funname," calculations\n")
+    }  
+  }
 
 # FUNCTION SPECIFIC ERROR CHECKING
 
@@ -66,6 +73,8 @@ utils.recalc.freqhets <- function(x, verbose=2) {
      t <- as.matrix(x)
      if (verbose >= 2) {cat("  Recalculating locus metric freqHets\n")}
      x@other$loc.metrics$FreqHets <-  colMeans(t==1, na.rm = T)
+     x@other$loc.metrics.flags$FreqHets <- TRUE
+     
 # FLAG SCRIPT END
 
   if (verbose > 0) {
