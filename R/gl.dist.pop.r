960236675240d5e3dd7eae819aa0556f870eafef
arthurgeorges <georges@aerg.canberra.edu.au>
2019-10-21 09:39:50 +1100

    Revised gl.dist.ind to add binary data distance measures 'simple matching', Jaccard, Dice (=Sorenson, Czekonowski), Phi (Pearson correlation). These are valid for SilicoDArT data. gl.dist.ind and gl.dist.pop now work on both SNP and SilicoDArT data.

--- R/gl.dist.pop.r
+++ R/gl.dist.pop.r
@@ -2,69 +2,67 @@
 #'
 #' This script calculates various distances between populations based on allele frequencies. The distances are
 #' calculated by scripts in the {stats} or {vegan} libraries, with the exception of the pcfixed (percent fixed
-#' differences) and pa (total private alleles) distances.
+#' differences) distance.
 #' 
 #' The distance measure can be one of "manhattan", "euclidean", "pcfixed", "pa", canberra", "bray", 
 #' "kulczynski", "jaccard", "gower", "morisita", "horn", "mountford", "raup" , 
 #' "binomial", "chao", "cao", "mahalanobis", "maximum", "binary" or "minkowski". Refer to the documentation for
 #' dist stats or vegdist vegan for definitions. 
 #' 
-#' Distance pcfixed calculates the pair-wise count of fixed allelic differences between populations. Distance pa
-#' tallies the total number of private alleles possessed by one or the other population.
+#' Distance pcfixed calculates the pair-wise count of fixed allelic differences between populations.
 #'
 #' @param x -- name of the genlight containing the SNP genotypes [required]
-#' @param method -- Specify distance measure [method=euclidean]
-#' @param binary -- Perform presence/absence standardization before analysis using decostand [binary=FALSE]
-#' @param diag -- Compute and display diagonal [FALSE]
-#' @param upper -- Return also upper triangle [FALSE]
+#' @param method -- Specify distance measure [euclidean]
+#' @param plot -- if TRUE, display a histogram of the genetic distances, and a whisker plot [TRUE]
+#' @param boxplot -- if 'standard', plots a standard box and whisker plot; if 'adjusted',
+#' plots a boxplot adjusted for skewed distributions ['standard']
+#' @param range -- specifies the range for delimiting outliers [1.5 interquartile ranges]
+#' @param binary -- Perform presence/absence standardization before analysis using decostand [FALSE]
 #' @param p -- The power of the Minkowski distance (typically a value ranging from 0.25 to infinity) [0.5]
-#' @param verbose -- verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
-#' @return A matrix of distances between populations (class dist)
+#' @param verbose -- verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [2]
+#' @return An object of class 'dist' giving distances between populations
 #' @importFrom stats dist
 #' @importFrom vegan vegdist
 #' @importFrom reshape2 dcast
 #' @export
 #' @author Arthur Georges (Post to \url{https://groups.google.com/d/forum/dartr})
 #' @examples
-#' gl.dist.pop(testset.gl, method="euclidean", diag=TRUE)
+#' gl.dist.pop(testset.gl, method="euclidean")
 
-# Last amended 3-Feb-19
+gl.dist.pop <- function(x, method="euclidean", plot=TRUE, boxplot="standard", range=1.5, binary=FALSE, p=NULL, verbose=2) {
 
-gl.dist.pop <- function(x, method="euclidean", binary=FALSE, diag=FALSE, upper=FALSE, p=NULL, verbose=2) {
-
-# TIDY UP FILE SPECS
-
+  # TIDY UP FILE SPECS
+  
   funname <- match.call()[[1]]
-
-# FLAG SCRIPT START
-
+  build <- "Jacob"
+  
+  # FLAG SCRIPT START
+  
   if (verbose < 0 | verbose > 5){
     cat("  Warning: Parameter 'verbose' must be an integer between 0 [silent] and 5 [full report], set to 2\n")
     verbose <- 2
   }
-
-  if (verbose > 0) {
-    cat("Starting",funname,"\n")
+  
+  if (verbose >= 1){
+    cat("Starting",funname,"[ Build =",build,"]\n")
   }
-
-# STANDARD ERROR CHECKING
+  
+  # STANDARD ERROR CHECKING
   
   if(class(x)!="genlight") {
-    cat("  Fatal Error: genlight object required!\n"); stop("Execution terminated\n")
+    stop("Fatal Error: genlight object required!\n")
   }
-
-
-  # Set a population if none is specified (such as if the genlight object has been generated manually)
-    if (is.null(pop(x)) | is.na(length(pop(x))) | length(pop(x)) <= 0) {
-      if (verbose >= 2){ cat("  Population assignments not detected, individuals assigned to a single population labelled 'pop1'\n")}
-      pop(x) <- array("pop1",dim = nInd(x))
-      pop(x) <- as.factor(pop(x))
-    }
-
-  # Check for monomorphic loci
-    tmp <- gl.filter.monomorphs(x, verbose=0)
-    if ((nLoc(tmp) < nLoc(x)) & verbose >= 2) {cat("  Warning: genlight object contains monomorphic loci\n")}
-
+  
+  if (all(x@ploidy == 1)){
+    if (verbose >= 2){cat("  Processing  Presence/Absence (SilicoDArT) data\n")}
+    data.type <- "SilicoDArT"
+  } else if (all(x@ploidy == 2)){
+    if (verbose >= 2){cat("  Processing a SNP dataset\n")}
+    data.type <- "SNP"
+  } else {
+    stop("Fatal Error: Ploidy must be universally 1 (fragment P/A data) or 2 (SNP data)")
+  }
+  
 # FUNCTION SPECIFIC ERROR CHECKING
 
 # DO THE JOB
@@ -74,18 +72,16 @@ gl.dist.pop <- function(x, method="euclidean", binary=FALSE, diag=FALSE, upper=F
                    "binomial", "chao", "cao", "mahalanobis")
   distmethod <- c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski", "Chebyshev")
 
-  if (!(method%in%veganmethod || method%in%distmethod || method=="pcfixed" || method == "pa")){
-    cat("Fatal Error: Specified distance method is not among those available\n")
-    cat("  Specify one of",veganmethod, distmethod,"\n")
-    stop("Terminating Execution")
+  if (!(method%in%veganmethod || method%in%distmethod || method=="pcfixed")){
+    stop("Fatal Error: Specified distance method is not among those available\n  Specify one of",veganmethod, distmethod,"\n")
   }
   hard.min.p <- 0.25
 
   m <- method
   b <- binary
-  d <- diag
-  u <- upper
   pr <- p
+  u <- TRUE
+  d <- TRUE
   
   # Calculate allele frequencies for each population and locus
     f <- gl.percent.freq(x,verbose=0)
@@ -97,6 +93,7 @@ gl.dist.pop <- function(x, method="euclidean", binary=FALSE, diag=FALSE, upper=F
     row.names(f)=f[,1]
     f <- f[,-c(1)]
     f <- f/100
+    
   # Calculate distance using dist {stat}
     if (m %in% distmethod) {
       if (verbose >= 2) {
@@ -123,7 +120,8 @@ gl.dist.pop <- function(x, method="euclidean", binary=FALSE, diag=FALSE, upper=F
       }
       dd <- stats::dist(f, method=m, diag=d, upper=u, p=pr)
     }
-    # Calculate distance using dist {stat}
+    
+    # Calculate distance using vegdist {vegan}
     if (m %in% veganmethod) {
       dd <- vegan::vegdist(f, method=m, binary=b, diag=d, upper=u, na.rm=TRUE)
       if (verbose >= 2) {
@@ -134,6 +132,7 @@ gl.dist.pop <- function(x, method="euclidean", binary=FALSE, diag=FALSE, upper=F
         if (verbose >= 2) {cat("  Note: the Bray-Curtis distance is non-metric, and so should be considered a dissimilarity measure. A metric alternative is the Jaccard distance.\n\n")}
       }
     }
+    
     if (m == "pcfixed"){
       dd <- gl.fixed.diff(x,verbose=0)[[3]]
       if (verbose >= 2) {
@@ -141,24 +140,24 @@ gl.dist.pop <- function(x, method="euclidean", binary=FALSE, diag=FALSE, upper=F
         cat("Note: this distance may be non-metric, and so should be considered a dissimilarity measure\n")
       }  
     }
-    if (m == "pa"){
-      dd <- gl.report.pa.pop(x)
-      dd <- dd[,c(3,4,10)]
-      tmp <- dd
-      tmp2 <- dd$pop1
-      dd$pop1 <- dd$pop2
-      dd$pop2 <- tmp2
-      dd <- rbind(dd,tmp)
-      dd <- dcast(dd, pop1 ~ pop2, value.var="totalpriv")
-      row.names(dd) <- dd[,1]
-      dd <- dd[,2:length(dd[1,])]
-      dd <- dd[order(row.names(dd)),]
-      dd <- dd[,order(colnames(dd))]
-      if (verbose >= 2) {
-        cat("  Calculating total private alleles\n")
-        cat("  Note: this distance may be non-metric, and so should be considered a dissimilarity measure\n")
-      }
-    }
+    # if (m == "pa"){
+    #   dd <- gl.report.pa.pop(x)
+    #   dd <- dd[,c(3,4,10)]
+    #   tmp <- dd
+    #   tmp2 <- dd$pop1
+    #   dd$pop1 <- dd$pop2
+    #   dd$pop2 <- tmp2
+    #   dd <- rbind(dd,tmp)
+    #   dd <- dcast(dd, pop1 ~ pop2, value.var="totalpriv")
+    #   row.names(dd) <- dd[,1]
+    #   dd <- dd[,2:length(dd[1,])]
+    #   dd <- dd[order(row.names(dd)),]
+    #   dd <- dd[,order(colnames(dd))]
+    #   if (verbose >= 2) {
+    #     cat("  Calculating total private alleles\n")
+    #     cat("  Note: this distance may be non-metric, and so should be considered a dissimilarity measure\n")
+    #   }
+    # }
     
     dd <- as.dist(dd) 
     
@@ -167,11 +166,71 @@ gl.dist.pop <- function(x, method="euclidean", binary=FALSE, diag=FALSE, upper=F
     mat <- as.matrix(dd)[ord, ord]
     dd <- as.dist(mat)
     
+# PLOT
+  if (plot){
+    
+    # Save the prior settings for mfrow, oma, mai and pty, and reassign
+    op <- par(mfrow = c(2, 1), oma=c(1,1,1,1), mai=c(0.5,0.5,0.5,0.5),pty="m")
+    
+    # Set margins for first plot
+    par(mai=c(1,0.5,0.5,0.5))
+    
+    # Plot Box-Whisker plot
+    if (all(x@ploidy==2)){
+      title <- paste0("SNP data (DArTSeq)\nPopulation ",method," Distances")
+    } else {
+      title <- paste0("Tag P/A data (SilicoDArT)\nPopulation ",method," Distances")
+    }  
+    
+    if (boxplot == "standard"){
+      boxplot(dd, horizontal=TRUE, col='red', range=range, main = title)
+      cat("  Standard boxplot, no adjustment for skewness\n")
+    } else {
+      robustbase::adjbox(dd,
+                         horizontal = TRUE,
+                         col='red',
+                         range=range,
+                         main = title)
+      cat("  Boxplot adjusted to account for skewness\n")
+    }  
+    
+    # Set margins for second plot
+    par(mai=c(0.5,0.5,0,0.5))
+    hist(dd, 
+         main="", 
+         xlab="", 
+         border="blue", 
+         col="red",
+         xlim=c(min(dd),max(dd)),
+         breaks=100)
+  }  
+  
+# SUMMARY 
+    # Print out some statistics
+  if(verbose >= 2){
+    cat("\n  Reporting inter-population distances\n")
+    cat("  Distance measure:",method,"\n")
+    cat("    No. of populations =", nPop(x), "\n")
+    cat("    Average no. of individuals per population =", nInd(x)/nPop(x), "\n")
+    cat("    No. of loci =", nLoc(x), "\n")
+    cat("    Miniumum Distance: ",round(min(dd),2),"\n")
+    cat("    Maximum Distance: ",round(max(dd),2),"\n")
+    cat("    Average Distance: ",round(mean(dd),3),"\n\n")
+  }  
+    
 # FLAG SCRIPT END
 
+  if (plot){
+    # Reset the par options    
+    par(op)
+  }
+    
   if (verbose > 0) {
     cat("Completed:",funname,"\n")
   }
     
     return(dd)
 }
+
+# # Test script
+D <- gl.dist.pop(testset.gl,boxplot='standard',verbose=3)
