6166c8727dce6cef07c73360aa901e99951b5dc9
luis mijangos <luis.mijangos@unsw.edu.au>
2021-05-11 17:40:30 +1000

    change place of colors to .onAttach

--- R/zzz.r
+++ R/zzz.r
@@ -1,13 +1,9 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(...) {
   error <- crayon::red 
   warn <- crayon::yellow
   report <- crayon::green
   important <- crayon::cyan 
   wes_palette_Zissou1 <- c("#3B9AB2" ,"#78B7C5" ,"#EBCC2A" ,"#E1AF00" ,"#F21A00")
-  invisible()
-}
-
-.onAttach <- function(...) {
   packageStartupMessage(important("**** Welcome to dartR ****\n"))
   packageStartupMessage(report("Be aware that owing to CRAN requirements and compatibility reasons not all functions of the packages may run yet, as some dependencies could be missing. Hence for a most enjoyable experience we recommend to run the function "))
   packageStartupMessage(report("gl.install.vanilla.dartR()"))                 
