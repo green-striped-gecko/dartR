f5f3b02f44d4272648c74d39a749cbd092fca3a4
luis mijangos <luis.mijangos@unsw.edu.au>
2021-05-12 10:35:29 +1000

    update .onAttach

--- R/zzz.r
+++ R/zzz.r
@@ -3,7 +3,7 @@
   warn <- crayon::yellow
   report <- crayon::green
   important <- crayon::blue 
-  code <- caryon::cyan
+  code <- crayon::cyan
   wes_palette_Zissou1 <- colorRampPalette(c("#3B9AB2" ,"#78B7C5" ,"#EBCC2A" ,"#E1AF00" ,"#F21A00"))
   packageStartupMessage(important("**** Welcome to dartR ****\n"))
   packageStartupMessage(report("Be aware that owing to CRAN requirements and compatibility reasons not all functions of the packages may run yet, as some dependencies could be missing. Hence for a most enjoyable experience we recommend to run the function "))
