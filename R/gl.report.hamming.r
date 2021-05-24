1caa121a038d72b8d2b72acede8785de9fcba5a4
luis mijangos <luis.mijangos@unsw.edu.au>
2021-05-25 07:37:15 +1000

    update plots and format gl.report.hamming

--- R/gl.report.hamming.r
+++ R/gl.report.hamming.r
@@ -1,13 +1,25 @@
-#' Calculates the pairwise Hamming distance between DArT trimmed DNA sequences
+#'@name gl.report.hamming
 #'
-#' Hamming distance is calculated as the number of base differences between two 
+#'@title Calculates the pairwise Hamming distance between DArT trimmed DNA sequences
+#'
+#'@description Hamming distance is calculated as the number of base differences between two 
 #' sequences which can be expressed as a count or a proportion. Typically, it is
 #' calculated between two sequences of equal length. In the context of DArT
 #' trimmed sequences, which differ in length but which are anchored to the left
 #' by the restriction enzyme recognition sequence, it is sensible to compare the
 #' two trimmed sequences starting from immediately after the common recognition
 #' sequence and terminating at the last base of the shorter sequence. 
-#' 
+#'
+#'@param x Name of the genlight object containing the SNP data [required]
+#'@param rs Number of bases in the restriction enzyme recognition sequence [default 5]
+#'@param threshold Minimum acceptable base pair difference for display on the boxplot and histogram [default 3 bp]
+#'@param taglength Typical length of the sequence tags [default 69]
+#'@param plot_theme Theme for the plot. See Details for options [default theme_dartR()].
+#'@param plot_colours List of two color names for the borders and fill of the plots [default two_colors].
+#'@param probar If TRUE, then a progress bar is displayed on long loops [default TRUE]
+#'
+#'@details The function \code{\link{gl.filter.hamming}} will filter out one of two loci if their Hamming distance is less than a specified percentage
+#'
 #' Hamming distance can be computed by exploiting the fact that the dot product 
 #' of two binary vectors x and (1-y) counts the corresponding elements that are 
 #' different between x and y. This approach can also be used for vectors that 
@@ -17,214 +29,192 @@
 #'
 #' The algorithm is that of Johann de Jong 
 #' \url{https://johanndejong.wordpress.com/2015/10/02/faster-hamming-distance-in-r-2/}
-#' as implimented in utils.hamming.r
+#' as implemented in utils.hamming.r
 #' 
-#' A histogram and whiskerplot can be requested. Both display a user specified value
-#' for the minumum acceptable Hamming distance.
+#' This function reports the
+#'  number of missing values for each of several quantiles. Quantiles are
+#'  partitions of a finite set of values into q subsets of (nearly) equal sizes.
+#'  In this function q = 20. Quantiles are useful measures because they are less
+#'  susceptible to long-tailed distributions and outliers.
+#'  
+#' The minimum, maximum, mean and a tabulation of call rate quantiles against
+#'  thresholds rate are provided. Output also includes a boxplot and a
+#'  histogram.
 #'
-#' @param x -- name of the genlight object containing the SNP data [required]
-#' @param rs -- number of bases in the restriction enzyme recognition sequence [default 5]
-#' @param boxplot -- if 'standard', plots a standard box and whisker plot; 
-#' if 'adjusted', plots a boxplot adjusted for skewed distributions [default 'adjusted']
-#' @param range -- specifies the range for delimiting outliers [default = 1.5 interquartile ranges]
-#' @param threshold minimum acceptable base pair difference for display on the whisker plot and histogram [default 3 bp]
-#' @param taglength -- typical length of the sequence tags [default 69]
-#' @param probar -- if TRUE, then a progress bar is desplayed on long loops [default TRUE]
-#' @param verbose -- verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2 or as specified using gl.set.verbosity]
-#' @return Tabulation of loc that will be lost on filtering, against values of the threshold
-#' @importFrom adegenet glPlot
-#' @importFrom graphics hist
-#' @importFrom stats sd
-#' @importFrom robustbase adjbox
-#' @importFrom graphics par
-#' @export
-#' @author Arthur Georges (Post to \url{https://groups.google.com/d/forum/dartr})
-#' @examples
+#'  \strong{Plots and table are saved to the temporal directory (tempdir) and
+#'  can be accessed with the function \code{\link{gl.access.report}}. Note that
+#'  they can be accessed only in the current R session because tempdir is
+#'  cleared each time that an R session is closed.}
+#'
+#'  Examples of other themes that can be used can be consulted in \itemize{
+#'  \item \url{https://ggplot2.tidyverse.org/reference/ggtheme.html} and \item
+#'  \url{https://yutannihilation.github.io/allYourFigureAreBelongToUs/ggthemes/}
+#'  }
+#' 
+#'@return Returns a genlight object with the file names of plots and table that
+#'  were saved in the tempdir stored in the slot other$history
+#'
+#'@author Arthur Georges (Post to \url{https://groups.google.com/d/forum/dartr})
+#'
+#'@examples
 #' out <- gl.report.hamming(testset.gl)
+#'
+#'@importFrom stats sd
+#'
+#'@export 
+#'
 
-gl.report.hamming <- function(x, 
-                              rs=5, 
-                              boxplot="adjusted",
-                              range=1.5,
-                              threshold=3,
-                              taglength=69,
-                              probar=FALSE, 
-                              verbose = 2) {
-  
-# TRAP COMMAND, SET VERSION
-  
-  funname <- match.call()[[1]]
-  build <- "Jacob"
-  
-# SET VERBOSITY
-  
-  if (is.null(verbose)){ 
-    if(!is.null(x@other$verbose)){ 
-      verbose <- x@other$verbose
-    } else { 
-      verbose <- 2
+gl.report.hamming <- function(x, rs = 5, threshold = 3, taglength = 69, plot_theme = theme_dartR(), plot_colours = two_colors, probar = FALSE) {
+
+    # TRAP COMMAND, SET VERSION
+
+    funname <- match.call()[[1]]
+
+    # GENERAL ERROR CHECKING
+
+    x <- utils.check.gl(x)
+
+    # FUNCTION SPECIFIC ERROR CHECKING
+
+    if (length(x@other$loc.metrics$TrimmedSequence) == 0) {
+        stop(error("Fatal Error: Data must include Trimmed Sequences\n"))
     }
-  } 
-  
-  if (verbose < 0 | verbose > 5){
-    cat(paste("  Warning: Parameter 'verbose' must be an integer between 0 [silent] and 5 [full report], set to 2\n"))
-    verbose <- 2
-  }
-  
-# FLAG SCRIPT START
-  
-  if (verbose >= 1){
-    if(verbose==5){
-      cat("Starting",funname,"[ Build =",build,"]\n")
+
+    if (rs < 0 | rs > taglength) {
+        stop(error("Fatal Error: Length of restriction enzyme recognition sequence must be greater than zero, and less that the maximum length of a sequence tag; usually it is less than 9\n"))
+    }
+
+    if (nLoc(x) == 1) {
+        stop(error("Fatal Error: Data must include more than one locus\n"))
+    }
+
+    # FLAG SCRIPT START
+
+    if (verbose >= 1) {
+        if (verbose == 5) {
+            cat(report("Starting", funname, "[ Build =", build, "]\n"))
+        } else {
+            cat(report("Starting", funname, "\n"))
+        }
+    }
+
+    # DO THE JOB
+
+    s <- as.character(x@other$loc.metrics$TrimmedSequence)
+    tld <- threshold/(taglength - rs)
+
+    if (probar) {
+        pb <- txtProgressBar(min = 0, max = 1, style = 3, initial = 0, label = "Working ....")
+        getTxtProgressBar(pb)
+    }
+
+    if (verbose >= 2) {
+        cat(report("  Hamming distance ranges from zero (sequence identity) to 1 (no bases shared at any position)\n"))
+        cat(report("  Calculating pairwise Hamming distances between trimmed Reference sequence tags\n"))
+    }
+
+    count = 0
+    nL <- nLoc(x)
+    d <- rep(NA, (((nL - 1) * nL)/2))
+
+    for (i in 1:(nL - 1)) {
+        for (j in ((i + 1):nL)) {
+            count <- count + 1
+            d[count] <- utils.hamming(s[i], s[j], r = rs)
+        }
+        if (probar) {
+            setTxtProgressBar(pb, i/(nL - 1))
+        }
+    }
+
+    # get title for plots
+    if (all(x@ploidy == 2)) {
+        title <- paste0("SNP data (DArTSeq)\nPairwise Hamming Distance between sequence tags")
     } else {
-      cat("Starting",funname,"\n")
+        title <- paste0("Fragment P/A data (SilicoDArT)\nPairwise Hamming Distance between sequence tags")
     }
-  }
-  
-# STANDARD ERROR CHECKING
-  
-  if(class(x)!="genlight") {
-    stop("Fatal Error: genlight object required!\n")
-  }
-  
-  if (all(x@ploidy == 1)){
-    cat("  Processing Presence/Absence (SilicoDArT) data\n")
-  } else if (all(x@ploidy == 2)){
-    cat("  Processing a SNP dataset\n")
-  } else {
-    stop("Fatal Error: Ploidy must be universally 1 (fragment P/A data) or 2 (SNP data)!")
-  }
-  
-# FUNCTION SPECIFIC ERROR CHECKING
-
-  if(length(x@other$loc.metrics$TrimmedSequence) == 0) {
-    stop("Fatal Error: Data must include Trimmed Sequences\n")
-  }
-
-  if (rs < 0 | rs > taglength){
-    cat("Fatal Error: Length of restriction enzyme recognition sequence must be greater than zero, and less that the maximum length of a sequence tag; usually it is less than 9\n"); stop()
-  }
     
-  if (nLoc(x) == 1){
-    stop("Fatal Error: Data must include more than one locus\n")
-  }
-
-# DO THE JOB
-
-  s <- as.character(x@other$loc.metrics$TrimmedSequence)
-  tld <- threshold/(taglength-rs)
-  if( probar ) {
-    pb <- txtProgressBar(min=0, max=1, style=3, initial=0, label="Working ....")
-    getTxtProgressBar(pb)
-  }
-
-  if (verbose >= 2){
-    cat("  Hamming distance ranges from zero (sequence identity) to 1 (no bases shared at any position)\n")
-    cat("  Calculating pairwise Hamming distances between trimmed Reference sequence tags\n")
-  }
-  
-  count=0
-  nL <- nLoc(x)
-  d <- rep(NA,(((nL-1)*nL)/2))
-  
-  for (i in 1:(nL-1)){
-    for (j in ((i+1):nL)){
-      count <- count + 1
-      d[count] <- utils.hamming(s[i],s[j],r=rs)
+    if (verbose >= 2) {
+            cat("  Plotting boxplot and histogram of Hamming distance, showing a threshold of", threshold, "bp [HD", round(tld, 2), "]\n\n")
     }
-    if (probar) {setTxtProgressBar(pb, i/(nL-1))}
-  }
+    
+    # Boxplot
+    p1 <- ggplot(as.data.frame(d),aes(y=d)) + 
+      geom_boxplot(color = plot_colours[1], fill = plot_colours[2]) + 
+      geom_hline(yintercept=tld,color="red",size=2) +
+      coord_flip() + 
+      plot_theme + 
+      xlim(range = c(-1, 1)) + ylim(0, 1) + 
+      ylab(" ") + 
+      theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) + 
+      ggtitle(title)
+    
+    # Histogram
+    p2 <- ggplot(as.data.frame(d),aes(x=d)) + 
+        geom_histogram(bins = 50, color =plot_colours[1], fill = plot_colours[2]) + 
+      geom_vline(xintercept=tld,color="red",size=2) +
+      coord_cartesian(xlim = c(0, 1)) + 
+      xlab("Hamming distance") + 
+      ylab("Count") + 
+      annotate(geom = "text", x = tld+0.2,
+               y = max(hist(d,breaks=seq(0, 1, by=1/50), plot=FALSE)$counts)*0.75,
+               label = paste("Threshold of\n", threshold, "bp [HD", round(tld, 2), "]"))+
+      plot_theme
 
-  # Prepare for plotting
-  if (all(x@ploidy==2)){
-    title <- paste0("SNP data (DArTSeq)\nPairwise Hamming Distance between sequence tags")
-  } else {
-    title <- paste0("Fragment P/A data (SilicoDArT)\nPairwise Hamming Distance between sequence tags")
-  }  
-  if (verbose >= 2) {
-    if (boxplot == "adjusted"){
-      cat("  Plotting box and whisker plot (adjusted for skewness) and histogram of Hamming distance, showing a threshold of",threshold,"bp [HD",round(tld,2),"]\n")
-    } else {
-      cat("  Plotting box and whisker plot (standard) and histogram of Hamming distance, showing a threshold of",threshold,"bp [HD",round(tld,2),"]\n")
-    }  
-  }
-  # Save the prior settings for mfrow, oma, mai and pty, and reassign
-  op <- par(mfrow = c(2, 1), oma=c(1,1,1,1), mai=c(0.5,0.5,0.5,0.5),pty="m")
-  # Set margins for first plot
-  par(mai=c(1,0.5,0.5,0.5))
-  # Plot Box-Whisker plot
-  if (boxplot == "standard"){
-    whisker <- boxplot(d, 
-                       horizontal=TRUE, 
-                       col='red', 
-                       range=range,
-                       ylim=c(0,1),
-                       main = title)
-    abline(v=tld,col="red")
-   } else {
-    whisker <- robustbase::adjbox(x=as.numeric(d),
-                                  horizontal = TRUE,
-                                  col='red',
-                                  range=range,
-                                  ylim=c(0,1),
-                                  main = title)
-    abline(v=tld,col="red")
-  }  
-  # Set margins for second plot
-  par(mai=c(0.5,0.5,0,0.5))  
-  # Plot Histogram
-  hst <- hist(d,
-       main="",
-       xlab="",
-       border="blue", 
-       col="red",
-       xlim=c(0,1),
-       breaks=100)
-  abline(v=tld,col="red")
-  text(tld,max(hst$counts),pos=4,offset=1,paste(threshold,"bp"))
-  
-   cat("  No. of loci =", nLoc(x), "\n")
-   cat("  No. of individuals =", nInd(x), "\n")
-   cat("    Miniumum Hamming distance: ",round(min(d),2),"\n")
-   cat("    Maximum Hamming distance: ",round(max(d),2),"\n")
-   cat(paste0("    Mean Hamming Distance ",round(mean(d),2),"+/-",round(sd(d),3)," SD\n\n"))
-   #n.outliers <- sum(d[d<=(threshold/(taglength-rs))])
-   n.outliers <- sum(d<=(threshold/(taglength-rs)))
-   cat("  No. of pairs with Hamming Distance less than or equal to",threshold,"base pairs:",n.outliers,"\n")
-
-   # Determine the loss of loci for a given filter cut-off
-   retained <- array(NA,21)
-   pc.retained <- array(NA,21)
-   filtered <- array(NA,21)
-   pc.filtered <- array(NA,21)
-   percentile <- array(NA,21)
-   for (index in 1:21) {
-     i <- (index-1)*5
-     percentile[index] <- i/100
-     retained[index] <- length(d[d >= percentile[index]])
-     pc.retained[index] <- round(retained[index]*100/((((nL-1)*nL)/2)),1)
-     filtered[index] <- ((((nL-1)*nL)/2)) - retained[index]
-     pc.filtered[index] <- 100 - pc.retained[index]
-   }
-   df <- cbind(percentile,retained,pc.retained,filtered,pc.filtered)
-   df <- data.frame(df)
-   colnames(df) <- c("Threshold", "Retained", "Percent", "Filtered", "Percent")
-   df <- df[order(-df$Threshold),]
-   rownames(df) <- NULL
-#   if (verbose >= 3){
-#     cat("Note: The data below are for pairwise distances between",nL,"loci, for which there are",((((nL-1)*nL)/2)), "distances\n")
-#     print(df)
-#   } 
-   
-   # Reset the par options    
-   par(op)   
-   
-# FLAG SCRIPT END
-
-   if (verbose >= 1) {
-     cat("Completed:",funname,"\n")
-   }
-   
-   return(df)
-
+    cat("  No. of loci =", nLoc(x), "\n")
+    cat("  No. of individuals =", nInd(x), "\n")
+    cat("    Miniumum Hamming distance: ", round(min(d), 2), "\n")
+    cat("    Maximum Hamming distance: ", round(max(d), 2), "\n")
+    cat(paste0("    Mean Hamming Distance ", round(mean(d), 2), "+/-", round(sd(d), 3), " SD\n\n"))
+    n.outliers <- sum(d <= (threshold/(taglength - rs)))
+    cat("  No. of pairs with Hamming Distance less than or equal to", threshold, "base pairs:", n.outliers, "\n\n")
+    
+    # Determine the loss of loci for a given threshold
+    # using quantiles
+    nl <- nLoc(x)
+    quantile_res <- quantile(d,probs = seq(0, 1, 1/20))
+    retained <- unlist(lapply(quantile_res, function(y) {
+      res <- sum(d >= y)
+    }))
+    pc.retained <- round(retained * 100/((((nL - 1) * nL)/2)),1)
+    filtered <- ((((nL - 1) * nL)/2)) - retained
+    pc.filtered <- 100 - pc.retained
+    df <- data.frame(as.numeric(sub("%", "", names(quantile_res))), 
+                     quantile_res, retained, pc.retained, filtered, 
+                     pc.filtered)
+    colnames(df) <- c("Quantile", "Threshold", 
+                      "Retained", "Percent", "Filtered", "Percent")
+    df <- df[order(-df$Quantile), ]
+    df$Quantile <- paste0(df$Quantile, "%")
+    rownames(df) <- NULL
+    
+    # printing outputs
+    p3 <- (p1/p2) + plot_layout(heights = c(1, 4))
+    print(p3)
+    print(df)
+    
+    # creating temp file names
+    temp_plot <- tempfile(pattern = "plot_")
+    temp_table <- tempfile(pattern = "table_")
+    
+    # saving to tempdir
+    saveRDS(p3, file = temp_plot)
+    saveRDS(df, file = temp_table)
+    
+    # ADD TO HISTORY
+    
+    nh <- length(x@other$history)
+    x@other$history[[nh + 1]] <- c(match.call(), temp_plot, 
+                                   temp_table)
+    
+    # FLAG SCRIPT END
+    
+    if (verbose >= 1) {
+      cat(report("\n\nCompleted:", funname, "\n\n"))
+    }
+    
+    cat(important(strwrap("Plots and table were saved to the temporal directory (tempdir) and can be accesed with the function gl.access.report(). Note that they can be accessed only in the current R session because tempdir is cleared each time that the R session is closed.")))
+    
+    invisible(x)
+    
 }
