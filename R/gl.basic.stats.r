f9dd0041ba56a960ad2d95fcb810d930b7d52aa2
luis mijangos <luis.mijangos@unsw.edu.au>
2021-10-05 09:29:52 +1100

    fixes Carlo's functions

--- R/gl.basic.stats.r
+++ R/gl.basic.stats.r
@@ -4,14 +4,14 @@
 #' @param gl -- a genlight object
 #' @param digits -- number of digits that should be returned
 #' @importFrom hierfstat genind2hierfstat basic.stats
-#' @return several tables and lists with all basic stats. Check \code{\link[hierfstat]{basic.stats}} for details.
-#' @export
+#' @return several tables and lists with all basic stats. Check
+#' \code{\link[hierfstat]{basic.stats}} for details.
 #' @author Bernd Gruber (bugs? Post to \url{https://groups.google.com/d/forum/dartr})
 #' @examples 
 #' gl.basic.stats(possums.gl)
+#' @export
 
-gl.basic.stats <- function(gl, digits=4)
-{
+gl.basic.stats <- function(gl, digits=4){
   out <- basic.stats(genind2hierfstat(gl2gi(gl, verbose = 0)), digits = digits)
   return(out)
 }
--- R/utils.n.var.invariant.r
+++ R/utils.n.var.invariant.r
@@ -1,36 +1,37 @@
-#' An utility script to calculate the number of variant and invariant sites by
+#' A utility script to calculate the number of variant and invariant sites by
 #' locus
 #'
-#' calculate the number of variant and invariant sites by locus and add them as
+#' Calculate the number of variant and invariant sites by locus and add them as
 #' columns in \code{loc.metrics}. This can be useful to conduct further
 #' filtering, for example where only loci with secondaries are wanted for
 #' phylogenetic analyses.
 #'
-#' Invariant sites are the site (nucleotide) that are not polymorphic. When the
+#' Invariant sites are the sites (nucleotide) that are not polymorphic. When the
 #' locus metadata supplied by DArT includes the sequence of the allele
 #' (\code{TrimmedSequence}), it is used by this function to estimate the number
 #' of sites that were sequenced in each tag (read). This script then subtracts
-#' the number of polymorphic sites. The final number of variant and invariant
-#' sites is the added to the table in \code{gl@others$loc.metrics}.
+#' the number of polymorphic sites. The length of the trimmed sequence 
+#' (lenTrimSeq), the number of variant (n.variant) and 
+#' invariant (n.invariant) sites are the added to the table in 
+#' \code{gl@others$loc.metrics}.
 #'
 #' \strong{NOTE}: It is important to realise that this function correctly
 #' estimates the number of variant and invariant sites only when it is executed on
 #' \code{genlight} objects before secondaries are removed.
 #'
-#'
-#' @param x -- name of the genlight object containing the SNP data [required]
-#' @param verbose -- verbosity: 0, silent or fatal errors; 1, begin and end; 2,
-#'   progress log ; 3, progress and results summary; 5, full report [default 2]
+#' @param x Name of the genlight object containing the SNP data [required].
+#' @param verbose Verbosity: 0, silent or fatal errors; 1, begin and end; 2,
+#'   progress log ; 3, progress and results summary; 5, full report [default NULL].
 #' @return The modified genlight object
 #' @author Carlo Pacioni (Post to \url{https://groups.google.com/d/forum/dartr})
 #' @seealso
 #' \code{\link{gl.filter.secondaries}},\code{\link{gl.report.heterozygosity}}
 #' @export
-#' @import data.table
 #' @examples
-#' #out <- utils.n.invariant(testset.gl)
+#' out <- utils.n.var.invariant(platypus.gl)
 
-utils.n.var.invariant <- function(x, verbose=NULL) {
+utils.n.var.invariant <- function(x,
+                                  verbose = NULL) {
   
   # SET VERBOSITY
   
@@ -39,35 +40,44 @@ utils.n.var.invariant <- function(x, verbose=NULL) {
   # FLAG SCRIPT START
   
   funname <- match.call()[[1]]
-  utils.flag.start(func=funname,build="Jody",v=verbose) # I'm not quite sure what the build here should be
+  utils.flag.start(func=funname,build="Jody",verbosity=verbose) # I'm not quite sure what the build here should be
   
   # CHECK DATATYPE 
   datatype <- utils.check.datatype(x,verbose=verbose)
   
   # FUNCTION SPECIFIC ERROR CHECKING
   
-  if (grepl(x@other$history, pattern = "gl.filter.secondaries")) {
-    warning("This function should be run before removing secondaries. A gl.filter.secondaries call was found in the history. This may cause the results to be incorrect")
+  if (any(grepl(x@other$history, pattern = "gl.filter.secondaries")==TRUE)) {
+   cat(warn("  Warning: This function should be run before removing secondaries. A gl.filter.secondaries call was found in the history. This may cause the results to be incorrect\n"))
   }
+  
+  if(isFALSE("TrimmedSequence" %in% names(x$other$loc.metrics))) {
+    stop(error("The column 'TrimmedSequence' is not present in x$other$loc.metrics, but it is needed for this function"))
+  }
+  
+  # DO THE JOB
+  
+  n.invariant <- CloneID <- TrimmedSequence <- n.variant <- lenTrimSeq <- NA
+  
   if(isFALSE("CloneID" %in% names(x$other$loc.metrics))) {
     # Extract the clone ID number
     a <- strsplit(as.character(x@other$loc.metrics$AlleleID),"\\|")
     b <- unlist(a)[ c(TRUE,FALSE,FALSE) ] # You may want to put these two lines in a FUN and use it here and in gl/report.scondaries
     x@other$loc.metrics$CloneID <- b
   }
-  if(isFALSE("TrimmedSequence" %in% names(x$other$loc.metrics))) {
-    stop("The column 'TrimmedSequence' is not present in x$other$loc.metrics, but it is needed for this function")
-  }
-  
-  
-  # DO THE JOB
-  
-  if (verbose >= 2) {cat("  Calculating n invariant sites\n")}
+
+  if (verbose >= 2) {
+    cat(report("  Calculating n invariant sites\n"))
+    }
   proc.data <- data.table(x$other$loc.metrics) # using data.table
   proc.data[, lenTrimSeq := nchar(as.character(TrimmedSequence))]
   proc.data[, n.variant := .N, by=CloneID]
   proc.data[, n.invariant := lenTrimSeq - n.variant]
   x@other$loc.metrics <- setDF(proc.data)
+  
+  # ADD TO HISTORY  
+  nh <- length(x@other$history)
+  x@other$history[[nh + 1]] <- match.call()
   
   # FLAG SCRIPT END
   
@@ -77,4 +87,5 @@ utils.n.var.invariant <- function(x, verbose=NULL) {
   
   return(x)
 }
+
 
