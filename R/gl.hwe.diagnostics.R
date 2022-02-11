cf7e89a158308cfd1a3d47998f701810d1d09764
Carlo Pacioni <carlo.pacioni@gmail.com>
2022-02-11 12:59:18 +1100

    initial formatting

--- R/gl.hwe.diagnostics.R
+++ R/gl.hwe.diagnostics.R
@@ -1,10 +1,67 @@
-library(dartR)
-library(data.table)
-library(ggplot2)
+#' @name gl.hwe.diagnostics
+#' @title Provides descriptive stats and plots to diagnose potential problems with H-W proportions
+#' @description
+#' Different causes may be responsible for lack of H-W proportions. This function helps diagnose potential problems
+#' @inheritParams gl.report.hwe
+#' @details
+#'  This function initially runs \code{gl.report.hwe} and reports the the ternary plots. 
+#'  the remaining outputs follow the recommendations from Waples (2015) paper.  
+#'  These include:
+#'  \itemize{
+#'  \item A bar plot with the distribution of thep-values of the HWE tests. 
+#'  These should be roughly uniform  across the equal-sized bins.
+#'  \item A bar plot with the observed and expected number of significant HWE tests
+#'   for the same locus in multiple populations (that is, the x-axis is whether 
+#'   a locus results significant in 1, 2, ..., n populations. 0 indicates the 
+#'   number of non-significant tests). The observed and expected number of HWE 
+#'   tests should be roughly similarly distributed (if they are significant by 
+#'   chance alone). 
+#'   \item A table where the number of observed and expected significant HWE tests 
+#'   are reported by each population, indicating whether these are due to 
+#'   heterozygosity excess or deficiency. These can be used to have a clue of 
+#'   potential problems (e.g. deficiency= Wahlund effect, null alleles, 
+#'   non-random sampling; excess=sex linkage or different selection between sexes, 
+#'   demographic changes/small Ne. See Table 1 in Wapples 2015). In the last two 
+#'   columns of the table, chisquare value and its associated p-value is reported. The chisquare 
+#'   is computed following Fisher's procedure for a global test. This basically 
+#'   tests whether there is at least one test that is truly significant in the 
+#'   series of tests conducted (De Meeûs et al 2009).
+#'
+#' @return A list with the plots and table.
+#' @author Custodian: Carlo Pacioni -- Post to
+#' \url{https://groups.google.com/d/forum/dartr}
+#' @examples
+#' gl.hwe.diagnostics(x = platypus.gl)
+#' @references
+#' \itemize{
+#' \item De Meeûs, T., Guégan, J.-F., Teriokhin, A.T., 2009. MultiTest V.1.2, a 
+#' program to binomially combine independent tests and performance comparison 
+#' with other related methods on proportional data. BMC Bioinformatics 10, 443-443.
+#' \item Fisher, R., 1970. Statistical methods for research workers Edinburgh: 
+#' Oliver and Boyd.
+#' \item Waples, R. S. (2015). Testing for Hardy–Weinberg proportions: have we
+#' lost the plot?. Journal of heredity, 106(1), 1-19.
+#' }
+#' @seealso \code{\link{gl.report.hwe}}
+#' @family 
+#' @export
+#' @import data.table
+#' @import ggplot2
 
 
 
 gl.hwe.diagnostics <- function(x, alpha_val=0.05, brk = seq(0, 1, 1 / 20)) {
+  # SET VERBOSITY
+  verbose <- gl.check.verbosity(verbose)
+  
+  # FLAG SCRIPT START
+  funname <- match.call()[[1]]
+  utils.flag.start(func = funname,
+                   build = "Jackson",
+                   verbosity = verbose)
+  
+  # CHECK DATATYPE
+  datatype <- utils.check.datatype(x, verbose = verbose)
   
   #### Function helpers ####
   hwe.dist.plot <- function(hweout, brk) {
@@ -12,6 +69,8 @@ gl.hwe.diagnostics <- function(x, alpha_val=0.05, brk = seq(0, 1, 1 / 20)) {
     ggplot(hweout, aes(Prob)) + geom_histogram(breaks=brk) +
       geom_hline(yintercept = nrow(hweout)/(length(brk)-1), col="red")
   }
+  
+  # DO THE JOB
   
   # abandoned this for now
   # FstvsFis.plot <- function(Fstats) {
