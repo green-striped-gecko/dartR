196720436b552eac6e1b8fd9f71ac9671f687dc0
Arthur Georges <arthur.georges@biomatix.com.au>
2022-02-18 21:13:20 +1100

    gl.pcoa() -- added extra examples, modified the test set. gl.pcoa.plot() -- fixed a bug when distance matricies are used.

--- R/gl.pcoa.plot.r
+++ R/gl.pcoa.plot.r
@@ -247,8 +247,9 @@ gl.pcoa.plot <- function(glPca,
     }
     
     # Assign the new population list if as.pop is specified
-    pop.hold <- pop(x)
-    if (!is.null(as.pop)) {
+    if(datatype %in% c("SNP","SilicoDArT")){
+        pop.hold <- pop(x)
+        if (!is.null(as.pop)) {
         if (as.pop %in% names(x@other$ind.metrics)) {
             pop(x) <- as.matrix(x@other$ind.metrics[as.pop])
             if (verbose >= 2) {
@@ -266,6 +267,7 @@ gl.pcoa.plot <- function(glPca,
                     "Fatal Error: individual metric assigned to 'pop' does not exist. Check names(gl@other$loc.metrics) and select again\n"
                 )
             )
+        }
         }
     }
     
