7fabd475417dd1eca0b2446292f244c2644b112c
Luis <luis.mijangos@gmail.com>
2022-07-18 17:31:02 +1000

    small fix

--- R/gl2geno.r
+++ R/gl2geno.r
@@ -54,99 +54,101 @@ gl2geno <- function(x,
     
     outfilespec <- file.path(outpath, outfile)
     
-    dat <- as.matrix(x)
-    n <-nInd(x)
-    L <-nLoc(x)
+    # dat <- as.matrix(x)
+    # n <-nInd(x)
+    # L <-nLoc(x)
+    # 
+    # # Convert allelic data into absence/presence data at each locus Results are stored in the 'dat.binary' object
+    # 
+    # if (datatype == "SilicoDArT") {
+    #     dat <- as.matrix(dat)
+    #     dat.binary <- NULL
+    #     for (j in 1:L) {
+    #         allele <- sort(unique(dat[, j]))
+    #         for (i in allele[allele >= 0]) {
+    #             dat.binary <- cbind(dat.binary, dat[, j] == i)
+    #         }
+    #         LL <- ncol(dat.binary)
+    #         ind <- which(dat[, j] < 0)
+    #         if (length(ind) != 0) {
+    #             dat.binary[ind, (LL - length(allele) + 2):LL] <- -9
+    #         }
+    #     }
+    # }
+    # 
+    # if (datatype == "SNP") {
+    #     dummy_final <- NULL
+    #     for (r in 1:nrow(dat)) {
+    #         dummy <- rbind(dat[r, ], dat[r, ])
+    #         index <- colSums(dummy, na.rm = T) == 2
+    #         dummy[, index] <- c(0, 2)
+    #         dummy <- ifelse(is.na(dummy), -9, dummy)
+    #         dummy <- ifelse(dummy == 0, 1, dummy)
+    #         dummy_final <- rbind(dummy_final, dummy)
+    #     }
+    #     
+    #     dat.binary <- NULL
+    #     for (j in 1:L) {
+    #         allele <-sort(unique(dummy_final[, j]))
+    #         for (i in allele[allele >= 0]) {
+    #             dat.binary <- cbind(dat.binary, dummy_final[, j] == i)
+    #         }
+    #         LL <- dim(dat.binary)[2]
+    #         ind <- which(dummy_final[, j] < 0)
+    #         if (length(ind) != 0) {
+    #             dat.binary[ind, (LL - length(allele) + 2):LL] <- -9
+    #         }
+    #     }
+    # }
+    # 
+    # # Compute a genotype count for each allele (0,1,2 or 9 for a missing value) The results are stored in 'genotype'
+    # 
+    # n <- nrow(dat.binary)
+    # L <- ncol(dat)
+    # LL <- ncol(dat.binary)
+    # 
+    # if (datatype == "SNP") {
+    #     n <-n / 2
+    #     genotype <- matrix(NA, nrow = n, ncol = LL)
+    #     for (i in 1:n) {
+    #         genotype[i, ] <- dat.binary[2 * i - 1, ] + dat.binary[2 * i, ]
+    #         genotype[i, (genotype[i, ] < 0)] <- NA
+    #     }
+    #     if (LL == 2 * L) {
+    #         genotype <- genotype[, seq(2, LL, by = 2)]
+    #     }
+    # }
+    # 
+    # if (datatype == "SilicoDArT") {
+    #     genotype <- dat.binary
+    #     for (i in 1:n) {
+    #         genotype[i, (genotype[i, ] < 0)] <- NA
+    #     }
+    #     if (LL == 2 * L) {
+    #         genotype <- genotype[, seq(2, LL, by = 2)]
+    #     }
+    # }
+    # 
+    # genotype[is.na(genotype)] <- 9
+    # lst.monomorphic <- apply(
+    #     genotype,
+    #     2,
+    #     FUN = function(x) {
+    #         length(unique(x[x != 9]))
+    #     }
+    # )
+    # 
+    # if (sum(lst.monomorphic == 1) > 0) {
+    #     if (verbose > 0) {
+    #         cat(warn(
+    #             "  Monomorphic alleles generated during conversion were removed. \n"
+    #         ))
+    #     }
+    #     genotype <- genotype[, lst.monomorphic > 1]
+    # }
     
-    # Convert allelic data into absence/presence data at each locus Results are stored in the 'dat.binary' object
-    
-    if (datatype == "SilicoDArT") {
-        dat <- as.matrix(dat)
-        dat.binary <- NULL
-        for (j in 1:L) {
-            allele <- sort(unique(dat[, j]))
-            for (i in allele[allele >= 0]) {
-                dat.binary <- cbind(dat.binary, dat[, j] == i)
-            }
-            LL <- ncol(dat.binary)
-            ind <- which(dat[, j] < 0)
-            if (length(ind) != 0) {
-                dat.binary[ind, (LL - length(allele) + 2):LL] <- -9
-            }
-        }
-    }
-    
-    if (datatype == "SNP") {
-        dummy_final <- NULL
-        for (r in 1:nrow(dat)) {
-            dummy <- rbind(dat[r, ], dat[r, ])
-            index <- colSums(dummy, na.rm = T) == 2
-            dummy[, index] <- c(0, 2)
-            dummy <- ifelse(is.na(dummy), -9, dummy)
-            dummy <- ifelse(dummy == 0, 1, dummy)
-            dummy_final <- rbind(dummy_final, dummy)
-        }
-        
-        dat.binary <- NULL
-        for (j in 1:L) {
-            allele <-sort(unique(dummy_final[, j]))
-            for (i in allele[allele >= 0]) {
-                dat.binary <- cbind(dat.binary, dummy_final[, j] == i)
-            }
-            LL <- dim(dat.binary)[2]
-            ind <- which(dummy_final[, j] < 0)
-            if (length(ind) != 0) {
-                dat.binary[ind, (LL - length(allele) + 2):LL] <- -9
-            }
-        }
-    }
-    
-    # Compute a genotype count for each allele (0,1,2 or 9 for a missing value) The results are stored in 'genotype'
-    
-    n <- nrow(dat.binary)
-    L <- ncol(dat)
-    LL <- ncol(dat.binary)
-    
-    if (datatype == "SNP") {
-        n <-n / 2
-        genotype <- matrix(NA, nrow = n, ncol = LL)
-        for (i in 1:n) {
-            genotype[i, ] <- dat.binary[2 * i - 1, ] + dat.binary[2 * i, ]
-            genotype[i, (genotype[i, ] < 0)] <- NA
-        }
-        if (LL == 2 * L) {
-            genotype <- genotype[, seq(2, LL, by = 2)]
-        }
-    }
-    
-    if (datatype == "SilicoDArT") {
-        genotype <- dat.binary
-        for (i in 1:n) {
-            genotype[i, (genotype[i, ] < 0)] <- NA
-        }
-        if (LL == 2 * L) {
-            genotype <- genotype[, seq(2, LL, by = 2)]
-        }
-    }
-    
-    genotype[is.na(genotype)] <- 9
-    lst.monomorphic <- apply(
-        genotype,
-        2,
-        FUN = function(x) {
-            length(unique(x[x != 9]))
-        }
-    )
-    
-    if (sum(lst.monomorphic == 1) > 0) {
-        if (verbose > 0) {
-            cat(warn(
-                "  Monomorphic alleles generated during conversion were removed. \n"
-            ))
-        }
-        genotype <- genotype[, lst.monomorphic > 1]
-    }
-    
+    genotype <- as.matrix(x)
+    genotype[is.na(genotype)] <- 9    
     # write lfmm
     write.table(
         genotype,
