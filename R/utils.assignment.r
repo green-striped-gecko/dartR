c294895c434c86e0f28f4caa7e9f29390ca846aa
Luis <luis.mijangos@gmail.com>
2022-07-29 19:07:57 +1000

    small fixes

--- R/utils.assignment.r
+++ R/utils.assignment.r
@@ -7,14 +7,13 @@
 #
 #' @param x Name of the genlight object containing the SNP data [required].
 #' @param unknown Name of the individual to be assigned to a population [required].
-#' @param inbreeding_par The inbreeding parameter [default 0]. 
-#' @param zero_prob [default 0.01].
+# @param inbreeding_par The inbreeding parameter [default 0].
 #' @param verbose Verbosity: 0, silent or fatal errors; 1, begin and end; 2,
 #' progress log; 3, progress and results summary; 5, full report
 #' [default 2, unless specified using gl.set.verbosity].
 #' @details
 #' This function is a re-implementation of the function multilocus_assignment
-#'  from package gstudio. 
+#'  from package gstudio.
 #'  Description of the method used in this function can be found at:
 #' https://dyerlab.github.io/applied_population_genetics/population-assignment.html
 #' @return A \code{data.frame} consisting of assignment probabilities for each
@@ -26,11 +25,9 @@
 #' @export
 
 utils.assignment <- function(x,
-                            unknown,
-                            inbreeding_par = 0,
-                            zero_prob = 0.001,
-                            verbose = 2) {
-  
+                             unknown,
+                             # inbreeding_par = 0,
+                             verbose = 2) {
   # SET VERBOSITY
   verbose <- gl.check.verbosity(verbose)
   
@@ -43,24 +40,26 @@ utils.assignment <- function(x,
   # CHECK DATATYPE
   datatype <- utils.check.datatype(x, verbose = verbose)
   
-  if(unknown %in% indNames(x) ==FALSE){
-    stop(error(paste("  Individual",unknown,"is not in the genlight object\n")))
+  if (unknown %in% indNames(x) == FALSE) {
+    stop(error(
+      paste("  Individual", unknown, "is not in the genlight object\n")
+    ))
   }
   
   # DO THE JOB
   
-  # filtering loci with all missing data by population 
-  x <- gl.filter.allna(x, by.pop = TRUE, verbose = 0)
-  unknown_pop <- gl.keep.ind(x,ind.list =unknown,verbose = 0 )
+  # filtering loci with all missing data by population
+  # x <- gl.filter.allna(x, by.pop = TRUE, verbose = 0)
+  unknown_pop <- gl.keep.ind(x, ind.list = unknown, verbose = 0)
   unknown_pop <- data.frame(gl2alleles(unknown_pop))
   
-  x <- gl.drop.ind(x,ind.list = unknown,verbose = 0)
+  x <- gl.drop.ind(x, ind.list = unknown, verbose = 0)
   
   pop_names <- popNames(x)
-
+  
   pop_list <- seppop(x)
   gl_alleles <- do.call(rbind, strsplit(x$loc.all, "/"))
-
+  
   frequencies <- lapply(pop_list, function(y) {
     freq_allele <- gl.alf(y)
     freqs_gl <-
@@ -74,59 +73,70 @@ utils.assignment <- function(x,
   })
   
   ret <- data.frame(Population = pop_names, Probability = 0)
-
+  
   for (popx in 1:nPop(x)) {
     prob <- 1
     
     if (verbose >= 2) {
-      cat(report(
-        "  Assigning individual",
-        unknown,
-        "against population",
-        pop_names[popx],
-        "\n"
-      ))
+      cat(
+        report(
+          "  Assigning individual",
+          unknown,
+          "against population",
+          pop_names[popx],
+          "\n"
+        )
+      )
     }
-      
-      popfreq <- frequencies[[popx]]
     
-      loc <-  as.data.frame(do.call(rbind,strsplit(unname(unlist(unknown_pop)), ":")))
-      colnames(loc) <- c("a1","a2")
-      
-      df_assign <- cbind(loc,popfreq)
-      df_assign$hom1 <- df_assign$Frequency1^2
-      df_assign$hom2 <- df_assign$Frequency2^2
-      df_assign$het <- 2*df_assign$Frequency1*df_assign$Frequency2
-      
-      df_assign[which(df_assign$a1 == df_assign$a2 & df_assign$a1 == df_assign$Allele1 ),c("hom2","het")] <- 0
-      
-      df_assign[which(df_assign$a1 == df_assign$a2 & df_assign$a1 == df_assign$Allele2 ),c("hom1","het")] <- 0
-      
-      df_assign[which(df_assign$a1 != df_assign$a2),c("hom1","hom2")] <- 0
-      
-      df_assign$prob <- df_assign$hom1+df_assign$hom2+df_assign$het
-      
-      df_assign[which(is.na(df_assign$a1)),"prob"] <- NA
-      
-      df_assign[which(df_assign$prob==0),"prob"] <- zero_prob
-      #   
-      #   if (inbreeding_par > 0) {
-      #     f <- f * (1 - inbreeding_par)
-      #     if (all_alleles[1] == all_alleles[2]) {
-      #       f <-
-      #         f + (popfreq$Frequency[popfreq$Allele == all_alleles[1]] * inbreeding_par)
-      #     }
-      #   }
-      #   
-   
+    popfreq <- frequencies[[popx]]
+    
+    loc <-
+      as.data.frame(do.call(rbind, strsplit(unname(
+        unlist(unknown_pop)
+      ), ":")))
+    colnames(loc) <- c("a1", "a2")
+    
+    df_assign <- cbind(loc, popfreq)
+    df_assign$hom1 <- df_assign$Frequency1 ^ 2
+    df_assign$hom2 <- df_assign$Frequency2 ^ 2
+    df_assign$het <- 2 * df_assign$Frequency1 * df_assign$Frequency2
+    
+    df_assign[which(df_assign$a1 == df_assign$a2 &
+                      df_assign$a1 == df_assign$Allele1), c("hom2", "het")] <-
+      0
+    
+    df_assign[which(df_assign$a1 == df_assign$a2 &
+                      df_assign$a1 == df_assign$Allele2), c("hom1", "het")] <-
+      0
+    
+    df_assign[which(df_assign$a1 != df_assign$a2), c("hom1", "hom2")] <-
+      0
+    
+    df_assign$prob <-
+      df_assign$hom1 + df_assign$hom2 + df_assign$het
+    
+    df_assign[which(is.na(df_assign$a1)), "prob"] <- NA
+    
+    df_assign[which(df_assign$prob == 0), "prob"] <- -1
+    
+    df_assign$prob <-    df_assign$prob / nLoc(x)
+    
+    #   if (inbreeding_par > 0) {
+    #     f <- f * (1 - inbreeding_par)
+    #     if (all_alleles[1] == all_alleles[2]) {
+    #       f <-
+    #         f + (popfreq$Frequency[popfreq$Allele == all_alleles[1]] * inbreeding_par)
+    #     }
+    #   }
+    #
+    
     # assign probability
-ret[popx,"Probability"] <- prod(df_assign$prob,na.rm = TRUE)
+    ret[popx, "Probability"] <- sum(df_assign$prob, na.rm = TRUE)
   }
   
   ret <- ret[order(-ret$Probability),]
-  
   ret$Posterior <- ret$Probability / sum(ret$Probability)
-  
   ret$Posterior <- round(ret$Posterior, 5)
   ret$Probability <- round(ret$Probability, 5)
   
