5f307102b2b17e902e502d2a658997f24eb2805a
Arthur Georges <arthur.georges@biomatix.com.au>
2022-08-15 07:06:02 +1000

    <<< errors

--- R/gl.tree.nj.r
+++ R/gl.tree.nj.r
@@ -49,7 +49,7 @@ gl.tree.nj <- function(x,
     
     # DO THE JOB
     
-<<<<<<< HEAD
+#<<<<<<< HEAD
     # # Convert gl object to a matrix of allele frequencies, locus by population
     # if (verbose >= 2) {
     #     cat(report(
@@ -68,7 +68,7 @@ gl.tree.nj <- function(x,
     
     d <- gl.dist.pop(x, method=dist.method, output="matrix")
     row.names(d) <- popNames(x)
-=======
+#=======
     # Convert gl object to a matrix of allele frequencies, locus by population
     if (verbose >= 2) {
         cat(report(
@@ -83,7 +83,7 @@ gl.tree.nj <- function(x,
     }
     d <- round(as.matrix(dist(t)), 4)
     # row.names(d) <- c(paste(row.names(d),' ')) row.names(d) <- substr(row.names(d),1,10)
->>>>>>> 06233be3b235c0270e437be3dcab853c38692903
+#>>>>>>> 06233be3b235c0270e437be3dcab853c38692903
     
     # Plot the distances as an nj tree
     tree <- ape::nj(d)
