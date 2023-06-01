4dd2dcfea96eb0dfad7971b60e0e26548f6c39f4
mijangos81 <luis.mijangos@gmail.com>
2023-06-01 10:42:30 +1000

    update sp version

--- R/gl.ld.haplotype.r
+++ R/gl.ld.haplotype.r
@@ -124,6 +124,16 @@ gl.ld.haplotype <- function(x,
     return(-1)
   }
   
+  pkg <- "sp"
+  if (!(requireNamespace(pkg, quietly = TRUE))) {
+    cat(error(
+      "Package",
+      pkg,
+      " needed for this function to work. Please install it.\n"
+    ))
+    return(-1)
+  }
+  
   # DO THE JOB
   
   group <- het <- lat <- long <- NULL
@@ -282,8 +292,8 @@ p <- NULL
           dissolve = TRUE
         )
       
-      polygon_haplo <- elide.polygonsdf_2(polygon_haplo,rotate = 45)
-      # polygon_haplo <- maptools::elide(polygon_haplo, rotate = 45)
+      # polygon_haplo <- elide.polygonsdf_2(polygon_haplo,rotate = 45)
+       polygon_haplo <- sp::elide(polygon_haplo, rotate = 45)
       polygon_haplo$id <- rownames(as.data.frame(polygon_haplo))
 
       #this only has the coordinates
@@ -309,8 +319,7 @@ p <- NULL
       dimnames(ld_matrix_3) <- NULL
       matrix_rotate <-
         rotate.matrix(x = ld_matrix_3,
-                      angle = -45,
-                      method = "simple")
+                      angle = -45)
       matrix_rotate_2 <- matrix_rotate
       matrix_rotate_2[matrix_rotate_2 == 0] <- NA
       matrix_rotate_3 <-
