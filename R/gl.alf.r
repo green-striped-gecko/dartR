c0c6ebffe24629fdd58ca68f37229d1f39d0994a
Luis Mijangos <mijangos@Luiss-MBP.diversityarrays.com>
2023-07-26 12:42:22 +1000

    test

--- R/gl.alf.r
+++ R/gl.alf.r
@@ -12,7 +12,7 @@
 #' barplot(t(as.matrix(gl.alf(possums.gl[,1:10]))))
 
 gl.alf <- function(x) {
-  alf <- colMeans(as.matrix(x), na.rm = T) / 2
+  alf <- colMeans(as.matrix(x), na.rm = TRUE) / 2
   out <- data.frame(alf1 = 1 - alf, alf2 = alf)
   return(out)
 }
